5dceb501141a70ddc29337371332b6e73215e3bb
Camrynne Sullivan <camrynne.sullivan@gmail.com>
2020-07-02 16:09:52 -0400

    Updated Pseudocode

--- Develop/script.js
+++ Develop/script.js
@@ -11,39 +11,71 @@ function writePassword() {
 
 //  Make Function Generate Password
 
-// make an array
+// We need a list of characters
 
 function generatePassword() {
-  var bucketLowercase = "abcdefghijklmnopqrstuvwxyz".split();
-  var bucketUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split();
-  var bucketNumber = "1234567890".split();
-  var bucketSpecialCharacters = "!@#$%^&*".split();
+  var bucketLowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+  var bucketUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  var bucketNumber = "1234567890".split("");
+  var bucketSpecialCharacters = "!@#$%^&*".split("");
 
-  var passwordLength = prompt(
-    "How many characters would you like? Pick a number between 8 and 128."
-  );
-  var passwordLowercase = confirm("Would you like lowercase letters??");
+  // We need to find out what the user wants
+
+  // Upper case letters, lower case, numbers, special characters
+
+  var passwordLength = prompt("Pick a password length  8 and 128.");
+  var passwordLowercase = confirm("Would you like lowercase letters?");
   var passwordUppercase = confirm("Would you like uppercase letters?");
   var passwordNumbers = confirm("Would you like numbers?");
   var passwordSpecialCharacters = confirm("Would you like special characters?");
 
-  if (passwordLength < 8 || passwordLength > 128) {
-    alert("Error: Please choose a number between 8 and 128");
-    return "";
+  //Make a list of characters based on what the user wants one master array
+
+  // If they ask for lowercase characters add lowercase
+
+  //if they ask for uppercase characters add uppercase
+
+  //if they ask for number add numbers
+
+  //if they ask for special characters add special characters
+
+  //depending on user input add length of characters
+
+  // Pick out the characters needed from the user input randomly
+
+  for (var i = 0; i < passwordLength; i++) {
+    if (passwordLowercase) {
+      passwordComplete +=
+        bucketLowercase[Math.floor(Math.random() * bucketLowercase.length)];
+    }
+
+    if (passwordLength < 8 || passwordLength > 128) {
+      alert("Error: Please choose a number between 8 and 128");
+      return "";
+    } else if (passwordLength >= 8 || passwordLength <= 128) {
+    }
+    // console.log(
+    //   passwordLength,
+    //   passwordLowercase,
+    //   passwordUppercase,
+    //   passwordNumbers,
+    //   passwordSpecialCharacters
+    // );
   }
 
-  console.log(
-    passwordLength,
-    passwordLowercase,
-    passwordUppercase,
-    passwordNumbers,
-    passwordSpecialCharacters
-  );
+  var passwordComplete = "";
 }
+
 // Pompt Alert for number characters
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//Pick out random items from list of characters
+
+//Put them all in one place -password
+
+// Send them back out
 
 // GIVEN I need a new, secure password
 // WHEN I click the button to generate a password
